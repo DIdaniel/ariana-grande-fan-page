1e47178bb51d9ebb014457f2f6354b539c8c0d07
KIM DONG IL <cappu159@gmail.com>
2021-07-10 23:26:35 +0900

    timer complete

--- src/components/featured/TimeUntil.jsx
+++ src/components/featured/TimeUntil.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect, useCallback } from "react";
 import { Slide } from "react-awesome-reveal";
 
 const TimeUntil = () => {
+  const [time, setTime] = useState({
+    days: "0",
+    hours: "0",
+    minutes: "0",
+    second: "0",
+  });
+
   const renderItem = (time, value) => (
     <div className="countdown_item">
       <div className="countdown_time">{time}</div>
@@ -9,15 +16,39 @@ const TimeUntil = () => {
     </div>
   );
 
+  const getTimeUntil = useCallback((deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time < 0) {
+      console.log("Date Passed");
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      // console.log(seconds, minutes, hours, days);
+
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => getTimeUntil("Dec, 20, 2021, 01:20:00"), 1000);
+  }, [getTimeUntil]);
+
   return (
     <Slide left delay={1000}>
       <div className="countdown_wrapper">
         <div className="countdown_top">Event Starts in</div>
         <div className="countdown_bottom">
-          {renderItem(27, "Days")}
-          {renderItem(4, "hs")}
-          {renderItem(10, "min")}
-          {renderItem(50, "sec")}
+          {renderItem(time.days, "Days")}
+          {renderItem(time.hours, "hs")}
+          {renderItem(time.minutes, "min")}
+          {renderItem(time.seconds, "sec")}
         </div>
       </div>
     </Slide>
